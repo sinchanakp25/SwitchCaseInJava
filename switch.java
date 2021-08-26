2c4e578d6fd6f4d2540e9374376d6ff06efa002e
sinchanakp25 <87449740+sinchanakp25@users.noreply.github.com>
2021-08-26 18:12:44 +0530

    Created switch.java

---
+++ switch.java
@@ -0,0 +1,22 @@
+package switchcase;
+import java.util.Scanner;
+public class Switchcase{
+  public static void main(String[] args){
+    int choice;
+    System.out.println("Pick one: 1.Hi\t2.Hey\t3.Hello\t");
+    Scanner s = new Scanner(System.in);
+    choice = s.nextInt();
+    switch(choice)
+    {
+      case 1 : System.out.println("You said Hi");
+               break;
+      case 2 : System.out.println("You said Hey");
+               break;
+      case 3 : System.out.println("You said Hello");
+               break;
+      default : System.out.println("Invalid Choice");
+    }
+  }
+}
+
+      
